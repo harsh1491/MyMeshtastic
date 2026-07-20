0d03c238b1778840721a804da4a06c5d6c02722e
harsh1491 <snharshh1491@gmail.com>
2026-07-20 15:54:57 +0530

    Help & documentation-Acknowledgement option made hidden

--- feature/settings/src/androidMain/kotlin/org/meshtastic/feature/settings/SettingsScreen.kt
+++ feature/settings/src/androidMain/kotlin/org/meshtastic/feature/settings/SettingsScreen.kt
@@ -277,14 +277,14 @@ fun SettingsScreen(
                 )
             }
 
-            ExpressiveSection(title = stringResource(Res.string.help_and_documentation)) {
-                ListItem(
-                    text = stringResource(Res.string.help_and_documentation),
-                    leadingIcon = MeshtasticIcons.HelpOutline,
-                ) {
-                    onNavigate(SettingsRoute.HelpDocs)
-                }
-            }
+//            ExpressiveSection(title = stringResource(Res.string.help_and_documentation)) {
+//                ListItem(
+//                    text = stringResource(Res.string.help_and_documentation),
+//                    leadingIcon = MeshtasticIcons.HelpOutline,
+//                ) {
+//                    onNavigate(SettingsRoute.HelpDocs)
+//                }
+//            }
 
 
             // ── ADD THIS BLOCK: Drone Alarm Settings Row ──
